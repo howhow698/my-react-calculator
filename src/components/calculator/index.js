14bfd4862f58cc2d34c1ecc133bb3cc0d950ed60
howhow698 <prestige698@gmail.com>
2020-12-16 23:32:38 +0800

    feat: add index in component folder for importing Screen, Keypad, Button component. install react-textfit for fitting texts into the element

---
+++ src/components/calculator/index.js
@@ -0,0 +1,11 @@
+import Screen from './Screen';
+import Keypad from './Keypad';
+import Button from './Button';
+
+const Calculator = {
+  Screen,
+  Keypad,
+  Button
+}
+
+export default Calculator;
