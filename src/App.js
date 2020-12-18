5c890b202ac493a0c32b2bb0bf53f8d1080751f0
howhow698 <prestige698@gmail.com>
2020-12-18 15:55:18 +0800

    feat: modify App.js for implement react redux store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { calculate } from './store/actions/calculate';
+import Calculator from './components/calculator';
+import * as fromCalculator from './store';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator.Screen {...this.props} />
+        <Calculator.Keypad {...this.props} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    calculateStatement: fromCalculator.getCalculationStatement(state),
+    total: fromCalculator.getTotal(state)
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    calculate: (buttonKey) => {
+      dispatch(calculate(buttonKey))
+    },
+  }
+}
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
