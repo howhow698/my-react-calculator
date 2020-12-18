e9ca0a8f8e30c905479afd8adf1259eefa01e6e0
howhow698 <prestige698@gmail.com>
2020-12-18 14:53:20 +0800

    feat: create Keypad component

---
+++ src/components/calculator/Keypad.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { keypadKeys } from '../../utils/calculate';
+import Calculator from '../calculator';
+
+export default class Keypad extends Component {
+  handleClick = (key) => {
+    this.props.calculate(key);
+  }
+  render() {
+    return (
+      keypadKeys.map(block => {
+        return block.map(key => {
+          <Calculator.Button
+            key={key}
+            onButtonClick={this.handleClick}
+            buttonKey={key}
+          />
+        });
+      })
+    );
+  }
+}
