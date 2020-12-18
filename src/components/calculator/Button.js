97da1950a5ee7f9f0e49202db2885445e1e18c12
howhow698 <prestige698@gmail.com>
2020-12-18 15:21:51 +0800

    feat: add Button component and constants

---
+++ src/components/calculator/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = ({ onButtonClick, buttonKey }) => {
+  let handleClick = () => { onButtonClick(buttonKey) }
+  return (
+    <button onClick={handleClick}>
+      { buttonKey}
+    </button>
+  );
+}
+
+export default Button;
